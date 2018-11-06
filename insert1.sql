2fb18dc1115258681be06f1d69e3062968b1264f
LPignata <luanpignat@gmail.com>
2018-11-06 13:39:48 -0200

    Criando scripts

---
+++ insert1.sql
@@ -0,0 +1,38 @@
+INSERT INTO mae (nome_mae)
+VALUE ("Maria");
+
+INSERT INTO pai (nome_pai)
+VALUE ("João");
+
+INSERT INTO sexo (desc_sexo)
+VALUE ("Masculino");
+
+INSERT INTO sexo (desc_sexo)
+VALUE ("Feminino");
+
+INSERT INTO estado_civil (desc_estado_civil)
+VALUE ("Solteiro");
+
+INSERT INTO estado_civil (desc_estado_civil)
+VALUE ("Casado");
+
+INSERT INTO estado_civil (desc_estado_civil)
+VALUE ("Separado");
+
+INSERT INTO estado_civil (desc_estado_civil)
+VALUE ("Viúvo");
+
+INSERT INTO pais (nome_pais)
+VALUE ("Brasil");
+
+INSERT INTO telefone (ddd_telefone, num_telefone)
+VALUE (61, 999887766);
+
+INSERT INTO escolaridade (desc_escolaridade)
+VALUE ("Ensino Médio");
+
+INSERT INTO escolaridade (desc_escolaridade)
+VALUE ("Ensino Superior");
+
+INSERT INTO cargo (desc_cargo)
+VALUE ("Programador");
